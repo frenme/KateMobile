3d7fedcb08783b26a74677fe38a7f46d14026acf
vikzelikov <vikzelikov@yandex.ru>
2020-01-03 02:56:33 +0300

    feat: add profile

--- app/src/main/java/bonch/dev/katemobile/Profile.kt
+++ app/src/main/java/bonch/dev/katemobile/Profile.kt
@@ -5,9 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
-class Profile: Fragment(){
+class Profile : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,6 +17,17 @@ class Profile: Fragment(){
     ): View? {
         val view: View =
             inflater.inflate(R.layout.profile_fragment, container, false)!!
+        val friends: TextView = view.findViewById(R.id.friends)
+        val stories: TextView = view.findViewById(R.id.stories)
+
+        friends.setOnClickListener {
+            Toast.makeText(context, "AAAAA", Toast.LENGTH_LONG).show()
+        }
+
+        stories.setOnClickListener {
+            Toast.makeText(context, "BBBBBBB", Toast.LENGTH_LONG).show()
+        }
+
 
         return view
     }
